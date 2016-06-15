f98c6f84e796fb42abfa857bb9e8913091c51ce2
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2016-06-15 15:42:08 +0500

    Hello World In React js

---
+++ app/Greeter.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Sufiyan on 6/15/2016.
+ */
+var greeter=require('./Greeter.js');
+
+document.getElementById('root').appendChild(greeter());
---
+++ app/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sufiyan on 6/15/2016.
+ */
+module.exports=function () {
+    var greet=document.createElement('div');
+    greet.textContent="Hi Sufiyan There and Greetings !";
+    return greet;
+
+};
